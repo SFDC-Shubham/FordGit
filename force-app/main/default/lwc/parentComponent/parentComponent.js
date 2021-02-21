24896e2f0d94ced5888a027d95b1c8cc33c3eeba
Shubham <Shubham.Khandelwal@team.telstra.com>
2021-02-21 20:17:35 +0530

    InitialCommit

---
+++ force-app/main/default/lwc/parentComponent/parentComponent.js
@@ -0,0 +1,13 @@
+import { LightningElement, track } from 'lwc';
+
+export default class ParentComponent extends LightningElement {
+    handleChangeEvent(event){
+        this.template.querySelector('c-child-Component').changeMessage(event.target.value);
+    }
+
+    @track msg;
+    handleCustomEvent(event) {
+        const textVal = event.detail;
+        this.msg = textVal;
+    }
+}
